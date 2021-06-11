c5e026307fbca9b71d47344db38048ea6f43bc49
ilhamharahap1 <52077174+ilhamharahap1@users.noreply.github.com>
2021-06-11 11:34:24 +0700

    Add files via upload

---
+++ Students.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Students extends Model
+{
+    use HasFactory; //ini bawaan default lararvel
+    //    protected $table = 'mahasiswa'; //ini berlaku ketika table yang di ambil bukan dari student, tetapidari table lain,
+}
